8b8c3b8ee46e381e63326b2b1a920afb67b547c6
Liam <160941195+billsyliamgta@users.noreply.github.com>
2025-06-09 10:59:27 +0100

    Update RenderTarget.cs

--- BillsyLiamGTA.UI/Elements/RenderTarget.cs
+++ BillsyLiamGTA.UI/Elements/RenderTarget.cs
@@ -5,6 +5,7 @@ namespace BillsyLiamGTA.UI.Elements
     public class RenderTarget
     {
         #region Fields
+            
         public int Handle { get; set; }
         
         public string Name { get; set; }
@@ -13,6 +14,8 @@ namespace BillsyLiamGTA.UI.Elements
 
         #endregion
 
+        #region Constructors
+            
         public RenderTarget(string name, int model)
         {
             Name = name;
@@ -30,6 +33,26 @@ namespace BillsyLiamGTA.UI.Elements
                 Handle = Function.Call<int>(Hash.GET_NAMED_RENDERTARGET_RENDER_ID, name);
             }
         }
+
+        public RenderTarget(string name, string model)
+        {
+            Name = name;
+            Model = Function.Call<int>(Hash.GET_HASH_KEY, model);
+            if (!Function.Call<bool>(Hash.IS_NAMED_RENDERTARGET_REGISTERED, name))
+            {
+                Function.Call(Hash.REGISTER_NAMED_RENDERTARGET, name, 0);
+            }
+            if (!Function.Call<bool>(Hash.IS_NAMED_RENDERTARGET_LINKED, model))
+            {
+                Function.Call(Hash.LINK_NAMED_RENDERTARGET, model);
+            }
+            if (Function.Call<bool>(Hash.IS_NAMED_RENDERTARGET_REGISTERED, name))
+            {
+                Handle = Function.Call<int>(Hash.GET_NAMED_RENDERTARGET_RENDER_ID, name);
+            }
+        }
+        
+        #endregion
         
         public void Release()
         {
